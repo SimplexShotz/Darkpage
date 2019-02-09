f9295e1dd6cfb1c4d5966fe6768cb7c09d17d6ba
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2019-02-08 19:56:23 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,28 +2,28 @@ window.addEventListener("load", function load(event) {
   document.getElementById("Standard").onclick = function() {
     chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function(tabs) {
       chrome.tabs.insertCSS(tabs[0].id, {
-        "code": "body,div,button { background-image: linear-gradient(rgb(50, 50, 50), rgb(50, 50, 50)) !important; } * { background-color: rgb(50, 50, 50) !important; border-color: rgb(100, 100, 100) !important; border-width: 1px !important; outline-color: rgb(50, 50, 50) !important; } * { color: rgb(200, 200, 200) !important; }"
+        "code": "body,div,button,span { background-image: linear-gradient(rgb(50, 50, 50), rgb(50, 50, 50)) !important; } * { background-color: rgb(50, 50, 50) !important; border-color: rgb(100, 100, 100) !important; border-width: 1px !important; outline-color: rgb(50, 50, 50) !important; } * { color: rgb(200, 200, 200) !important; }"
       });
     });
   };
   document.getElementById("Dark").onclick = function() {
     chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function(tabs) {
       chrome.tabs.insertCSS(tabs[0].id, {
-        "code": "body,div,button { background-image: linear-gradient(rgb(0, 0, 0), rgb(0, 0, 0)) !important; } * { background-color: rgb(0, 0, 0) !important; border-color: rgb(200, 200, 200) !important; border-width: 1px !important; outline-color: rgb(0, 0, 0) !important; } * { color: rgb(255, 255, 255) !important; }"
+        "code": "body,div,button,span { background-image: linear-gradient(rgb(0, 0, 0), rgb(0, 0, 0)) !important; } * { background-color: rgb(0, 0, 0) !important; border-color: rgb(200, 200, 200) !important; border-width: 1px !important; outline-color: rgb(0, 0, 0) !important; } * { color: rgb(255, 255, 255) !important; }"
       });
     });
   };
   document.getElementById("Night").onclick = function() {
     chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function(tabs) {
       chrome.tabs.insertCSS(tabs[0].id, {
-        "code": "body,div,button { background-image: linear-gradient(rgb(50, 50, 0), rgb(50, 50, 0)) !important; } * { background-color: rgb(50, 50, 0) !important; border-color: rgb(100, 100, 0) !important; border-width: 1px !important; outline-color: rgb(50, 50, 0) !important; } * { color: rgb(200, 200, 0) !important; }"
+        "code": "body,div,button,span { background-image: linear-gradient(rgb(50, 50, 0), rgb(50, 50, 0)) !important; } * { background-color: rgb(50, 50, 0) !important; border-color: rgb(100, 100, 0) !important; border-width: 1px !important; outline-color: rgb(50, 50, 0) !important; } * { color: rgb(200, 200, 0) !important; }"
       });
     });
   };
   document.getElementById("Sky").onclick = function() {
     chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function(tabs) {
       chrome.tabs.insertCSS(tabs[0].id, {
-        "code": "body,div,button { background-image: linear-gradient(rgb(50, 200, 200), rgb(50, 200, 200)) !important; } * { background-color: rgb(50, 200, 200) !important; border-color: rgb(0, 150, 150) !important; border-width: 1px !important; outline-color: rgb(50, 200, 200) !important; } * { color: rgb(255, 255, 255) !important; }"
+        "code": "body,div,button,span { background-image: linear-gradient(rgb(50, 200, 200), rgb(50, 200, 200)) !important; } * { background-color: rgb(50, 200, 200) !important; border-color: rgb(0, 150, 150) !important; border-width: 1px !important; outline-color: rgb(50, 200, 200) !important; } * { color: rgb(255, 255, 255) !important; }"
       });
     });
   };
